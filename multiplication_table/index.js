21b8f3a65808a9d72e2e9c0189cca51dfb10b874
howevert <1413537501@qq.com>
2020-06-16 10:49:25 +0800

    finish the tasks

---
+++ multiplication_table/index.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=9;i++){
+    let temp="";
+    for(let j=1;j<=i;j++){
+        //console.log(i+'*'+j+'='+i*j+' ');
+        temp+=i+'*'+j+'='+i*j+' ';
+    }
+    console.log(temp);
+}
+    
